2684aa459467512635681b56a065120ae0361697
ichigo <ichigomady@gmail.com>
2023-09-19 18:24:14 +0530

    fix margins and mismatch password logic

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ document.querySelector("form").addEventListener('submit', (e) => {
     pwd = document.getElementById('password');
     confirm_pwd = document.getElementById('confirmPassword');
 
+    // remove validation if exists
+    if(pwd.classList.contains('invalid') || confirm_pwd.classList.contains('invalid')){
+        pwd.classList.remove('invalid');
+        confirm_pwd.classList.remove('invalid');
+        document.getElementById('validation').textContent = "";
+    }
+
     if(!(pwd.value === confirm_pwd.value)){
         document.getElementById('validation').textContent = "*Passwords do not match";
         pwd.classList.add("invalid");
         confirm_pwd.classList.add("invalid");
     }
-
-
 })
